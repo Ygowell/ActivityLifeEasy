8d61db4862f7e57f120cca394ec951b33dff56b9
yangguowei <yangguowei_0115@163.com>
2018-02-09 12:34:56 +0800

    Activity1跳转到Activity2，如果Activity2的主题设置为透明时，Activity1将不会调用onStop，且返回的时候也不会调用onRestart

---
+++ app/src/main/java/com/jam/activitylifeeasy/Main2Activity.java
@@ -0,0 +1,53 @@
+package com.jam.activitylifeeasy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends Activity {
+
+    private final static String TAG = "Main2Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+}
